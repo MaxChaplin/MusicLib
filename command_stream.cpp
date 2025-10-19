7f47d3520af6a239e62632c90cc382cda3102cca
Max <max@maxubuntu.virtualbox.org>
2025-10-19 22:44:58 +0300

    The command stream is based on unique pointers now

--- command_stream.cpp
+++ command_stream.cpp
@@ -2,22 +2,75 @@
 #include "command_stream.hpp"
 #include "instrument.hpp"
 
-#include <iostream>
-
 namespace MusicLib {
     
-CommandStreamBasic::CommandStreamBasic(std::vector<std::shared_ptr<Command>>& commands, bool looping)
-: m_commands{commands}
+CommandStreamBasic::CommandStreamBasic(bool looping)
+: m_commands{}
 , m_cursor{0}
 , m_looping{looping}
-, m_finished{false}
 {
 
 }
 
-Command& CommandStreamBasic::current()
+CommandStreamBasic::CommandStreamBasic(std::vector<std::unique_ptr<Command>>& commands, bool looping)
+: m_commands{}
+, m_cursor{0}
+, m_looping{looping}
 {
-    return *m_commands[m_cursor];
+    m_commands.reserve(commands.size());
+
+    for (const auto& c : commands)
+    {
+        m_commands.push_back(c->clone());
+    }
+}
+
+
+CommandStreamBasic::CommandStreamBasic(const CommandStreamBasic& other)
+: m_commands{}
+, m_cursor{other.m_cursor}
+, m_looping{other.m_looping}
+{
+    m_commands.reserve(other.m_commands.size());
+
+    for (const auto& c : other.m_commands)
+    {
+        m_commands.push_back(c->clone());
+    }
+}
+
+CommandStreamBasic& CommandStreamBasic::operator=(const CommandStreamBasic& other)
+{
+    if (this != &other)
+    {
+        m_commands.clear();
+        m_commands.reserve(other.m_commands.size());
+        
+        for (const auto& c : other.m_commands)
+        {
+            m_commands.push_back(c->clone());
+        }
+
+        m_cursor = other.m_cursor;
+        m_looping = other.m_looping;
+    }
+
+    return *this;
+}
+
+void CommandStreamBasic::add(Command& command)
+{
+    m_commands.push_back(command.clone());
+}
+
+Command* CommandStreamBasic::current()
+{
+    if (m_cursor >= m_commands.size())
+    {
+        return nullptr;
+    }
+    
+    return m_commands[m_cursor].get();
 }
 
 unsigned long CommandStreamBasic::step()
@@ -32,10 +85,6 @@ unsigned long CommandStreamBasic::step()
         {
             m_cursor = 0;
         }
-        else
-        {
-            m_finished = true;
-        }
     }
 
     return m_cursor;
@@ -47,27 +96,88 @@ void CommandStreamBasic::cursor(unsigned long cursor)
     {
         m_cursor = cursor;
     }
-
-    m_finished = cursor >= m_commands.size() - 1;
 }
 
 bool CommandStreamBasic::finished()
 {
-    return m_finished;
+    return !m_looping && m_cursor >= m_commands.size() - 1;
 }
 
-CommandStreamInstrument::CommandStreamInstrument(std::vector<std::shared_ptr<Command>> commands, Instrument& ins, bool looping)
-: m_commands{commands}
+CommandStreamInstrument::CommandStreamInstrument(Instrument& ins, bool looping)
+: m_commands{}
+, m_cursor{0}
 , m_looping{looping}
-, m_finished{false}
 , m_ins{ins}
 {
 
 }
 
-Command& CommandStreamInstrument::current()
+CommandStreamInstrument::CommandStreamInstrument(std::vector<std::unique_ptr<Command>>& commands, Instrument& ins, bool looping)
+: m_commands{}
+, m_cursor{0}
+, m_looping{looping}
+, m_ins{ins}
 {
-    return *m_commands[m_cursor];
+    m_commands.reserve(commands.size());
+
+    for (const auto& c : commands)
+    {
+        m_commands.push_back(c->clone());
+    }
+}
+
+CommandStreamInstrument::CommandStreamInstrument(const CommandStreamInstrument& other)
+: m_commands{}
+, m_cursor{other.m_cursor}
+, m_looping{other.m_looping}
+, m_ins{other.m_ins}
+{
+    m_commands.reserve(other.m_commands.size());
+
+    for (const auto& c : other.m_commands)
+    {
+        m_commands.push_back(c->clone());
+    }
+
+    m_cursor = other.m_cursor;
+    m_looping = other.m_looping;
+    m_ins = other.m_ins;
+}
+
+CommandStreamInstrument& CommandStreamInstrument::operator=(const CommandStreamInstrument& other)
+{
+    if (this != &other)
+    {
+        m_commands.clear();
+        m_commands.reserve(other.m_commands.size());
+        
+        for (const auto& c : other.m_commands)
+        {
+            m_commands.push_back(c->clone());
+        }
+
+        m_cursor = other.m_cursor;
+        m_looping = other.m_looping;
+        m_ins = other.m_ins;
+    }
+
+    return *this;
+}
+
+
+void CommandStreamInstrument::add(Command& command)
+{
+    m_commands.push_back(command.clone());
+}
+
+Command* CommandStreamInstrument::current()
+{
+    if (m_cursor >= m_commands.size())
+    {
+        return nullptr;
+    }
+
+    return m_commands[m_cursor].get();
 }
 
 unsigned long CommandStreamInstrument::step()
@@ -82,10 +192,6 @@ unsigned long CommandStreamInstrument::step()
         {
             m_cursor = 0;
         }
-        else
-        {
-            m_finished = true;
-        }
     }
 
     return m_cursor;
@@ -97,13 +203,11 @@ void CommandStreamInstrument::cursor(unsigned long cursor)
     {
         m_cursor = cursor;
     }
-
-    m_finished = cursor >= m_commands.size() - 1;
 }
 
 bool CommandStreamInstrument::finished()
 {
-    return m_finished;
+    return !m_looping && m_cursor >= m_commands.size() - 1;
 }
 
 void CommandStreamInstrument::instrument(Instrument& ins)
